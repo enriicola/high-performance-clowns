e274aee3b5900b7ac11f0b5589dc4cfe50802c53
Christian Parodi <S4951409@studenti.unige.it>
2025-01-13 09:44:08 +0100

    Niente alla fine si confronta versione vanilla in parallelo e versione MPI sequenziale (credo non sto capendo niente)

--- hw3/src/pi_homework.c
+++ hw3/src/pi_homework.c
@@ -8,18 +8,25 @@
 #define INTERVALS 100000000000
 #endif
 
+#define NTHREADS 20
+
 int main(int argc, char **argv) {
   long long int i, intervals = INTERVALS;
   double x, dx, f, sum, pi;
   time_t time1, time2;
 
+#ifdef PARALLEL
+  time1 = omp_get_wtime();
+#else
   time1 = clock();
-
-  printf("Number of intervals: %lld\n", intervals);
+#endif
 
   sum = 0.0;
   dx = 1.0 / (double)intervals;
 
+#ifdef PARALLEL
+#pragma omp parallel for num_threads(NTHREADS) private(x, f) reduction(+ : sum)
+#endif
   for (i = 1; i <= intervals; i++) {
     x = dx * ((double)(i - 0.5));
     f = 4.0 / (1.0 + x * x);
@@ -28,7 +35,11 @@ int main(int argc, char **argv) {
 
   pi = dx * sum;
 
+#ifdef PARALLEL
+  time2 = omp_get_wtime() - time1;
+#else
   time2 = (clock() - time1) / (double)CLOCKS_PER_SEC;
+#endif
 
   printf("Computed PI %.24f\n", pi);
   printf("The true PI %.24f\n\n", PI25DT);
