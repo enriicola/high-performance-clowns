e77844165402be93c2753f2ee95b6cca6da4a955
Christian Parodi <S4951409@studenti.unige.it>
2025-02-13 14:35:40 +0100

    Add parallel execution support and timing for heat simulation

--- hw2/src/heat.c
+++ hw2/src/heat.c
@@ -1,36 +1,70 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>  // Added for time measurement
 
 // Simple define to index into a 1D array from 2D space
 #define I2D(num, c, r) ((r) * (num) + (c))
+
+// Colab maximum NTHREADS is 2
+#ifndef NTHREADS
+#define NTHREADS 2
+#endif
 
 #ifndef SIZE
 #define SIZE 1000
 #endif
 
-void step_kernel_ref(const size_t ni, const size_t nj, const float fact, const float* temp_in, float* temp_out) {
-  size_t i00, im10, ip10, i0m1, i0p1;
-  float d2tdx2, d2tdy2;
-
+/*
+ * `step_kernel_mod` is currently a direct copy of the CPU reference solution
+ * `step_kernel_ref` below. Accelerate it to run as a CUDA kernel.
+ */
+void step_kernel_mod(int ni, int nj, float fact, float* temp_in, float* temp_out) {
+#ifdef PARALLEL
+#pragma omp parallel for num_threads(NTHREADS)
+#endif
   // loop over all points in domain (except boundary)
-  for (size_t j = 1; j < nj - 1; j++) {
-    for (size_t i = 1; i < ni - 1; i++) {
+  for (int j = 1; j < nj - 1; j++) {
+    for (int i = 1; i < ni - 1; i++) {
       // find indices into linear memory
       // for central point and neighbours
-      i00 = I2D(ni, i, j);
-      im10 = I2D(ni, i - 1, j);
-      ip10 = I2D(ni, i + 1, j);
-      i0m1 = I2D(ni, i, j - 1);
-      i0p1 = I2D(ni, i, j + 1);
+      int ij = I2D(ni, i, j);
+      int im1j = I2D(ni, i - 1, j);
+      int ip1j = I2D(ni, i + 1, j);
+      int ijm1 = I2D(ni, i, j - 1);
+      int ijp1 = I2D(ni, i, j + 1);
 
       // evaluate derivatives
-      d2tdx2 = temp_in[im10] - 2 * temp_in[i00] + temp_in[ip10];
-      d2tdy2 = temp_in[i0m1] - 2 * temp_in[i00] + temp_in[i0p1];
+      float dx2 = temp_in[im1j] - 2 * temp_in[ij] + temp_in[ip1j];
+      float dy2 = temp_in[ijm1] - 2 * temp_in[ij] + temp_in[ijp1];
 
       // update temperatures
-      temp_out[i00] = temp_in[i00] + fact * (d2tdx2 + d2tdy2);
+      temp_out[ij] = temp_in[ij] + fact * (dx2 + dy2);
+    }
+  }
+}
+
+void step_kernel_ref(int ni, int nj, float fact, float* temp_in, float* temp_out) {
+  int ij, im1j, ip1j, ijm1, ijp1;
+  float dx2, dy2;
+
+  // loop over all points in domain (except boundary)
+  for (int j = 1; j < nj - 1; j++) {
+    for (int i = 1; i < ni - 1; i++) {
+      // find indices into linear memory
+      // for central point and neighbours
+      ij = I2D(ni, i, j);
+      im1j = I2D(ni, i - 1, j);
+      ip1j = I2D(ni, i + 1, j);
+      ijm1 = I2D(ni, i, j - 1);
+      ijp1 = I2D(ni, i, j + 1);
+
+      // evaluate derivatives
+      dx2 = temp_in[im1j] - 2 * temp_in[ij] + temp_in[ip1j];
+      dy2 = temp_in[ijm1] - 2 * temp_in[ij] + temp_in[ijp1];
+
+      // update temperatures
+      temp_out[ij] = temp_in[ij] + fact * (dx2 + dy2);
     }
   }
 }
@@ -46,21 +80,21 @@ int main() {
 
   const size_t size = ni * nj * sizeof(float);
 
-  printf("MATRIX SIZE: %zux%zu = %zu B = %.3f GB\n", ni, nj, size, (double)size / 1e9);
+  printf("Matrix size: %zux%zu (%.3f GB)\n", ni, nj, (double)size / 1e9);
 
-  double start = clock();
+  double time1 = clock();
 
   temp1_ref = (float*)malloc(size);
   temp2_ref = (float*)malloc(size);
   temp1 = (float*)malloc(size);
   temp2 = (float*)malloc(size);
 
-  // init with random data
-  for (size_t i = 0; i < ni * nj; ++i) {
+  // Initialize with random data
+  for (int i = 0; i < ni * nj; ++i) {
     temp1_ref[i] = temp2_ref[i] = temp1[i] = temp2[i] = (float)rand() / (float)(RAND_MAX / 100.0f);
   }
 
-  // CPU execution
+  // Execute the CPU-only reference version
   for (istep = 0; istep < nstep; istep++) {
     step_kernel_ref(ni, nj, tfac, temp1_ref, temp2_ref);
 
@@ -70,9 +104,19 @@ int main() {
     temp2_ref = temp_tmp;
   }
 
+  // Execute the modified version using same data
+  for (istep = 0; istep < nstep; istep++) {
+    step_kernel_mod(ni, nj, tfac, temp1, temp2);
+
+    // swap the temperature pointers
+    temp_tmp = temp1;
+    temp1 = temp2;
+    temp2 = temp_tmp;
+  }
+
   float maxError = 0;
   // Output should always be stored in the temp1 and temp1_ref at this point
-  for (size_t i = 0; i < ni * nj; ++i) {
+  for (int i = 0; i < ni * nj; ++i) {
     if (abs(temp1[i] - temp1_ref[i]) > maxError) {
       maxError = abs(temp1[i] - temp1_ref[i]);
     }
@@ -84,13 +128,14 @@ int main() {
   else
     printf("The Max Error of %.5f is within acceptable bounds.\n", maxError);
 
+  double time2 = 1000.0 * (clock() - time1) / (double)CLOCKS_PER_SEC;
+
+  printf("Elapsed time: %f ms\n", time2);
+
   free(temp1_ref);
   free(temp2_ref);
   free(temp1);
   free(temp2);
 
-  double end = (clock() - start) / (double)CLOCKS_PER_SEC * 1000.0;  // milliseconds
-
-  printf("\nElapsed time = %.2lf ms = %.2lf s\n", end, end / 1000.0);
   return 0;
 }
